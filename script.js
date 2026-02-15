7d190b331fcf7fdef161e244ac7f494efe2c4584
Your Name <jessekiplangat06@gmail.com>
2026-02-15 16:58:06 +0300

    Website update

---
+++ script.js
@@ -0,0 +1,24 @@
+const carouselSlide = document.querySelector('.carousel-slide');
+const carouselImages = document.querySelectorAll('.carousel-slide img');
+
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+
+let counter = 0;
+const size = carouselImages[0].clientWidth;
+
+function updateCarousel() {
+  carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+}
+
+nextBtn.addEventListener('click', () => {
+  counter++;
+  if(counter >= carouselImages.length) counter = 0;
+  updateCarousel();
+});
+
+prevBtn.addEventListener('click', () => {
+  counter--;
+  if(counter < 0) counter = carouselImages.length - 1;
+  updateCarousel();
+});
